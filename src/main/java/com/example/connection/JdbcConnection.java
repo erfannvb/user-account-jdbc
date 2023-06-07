25917bcdec3eb4ae56f1031dde4e923a9cc874c7
Erfan <erfannavab@gmail.com>
2023-06-07 13:25:29 +0330

    - defined database - added database connection

---
+++ src/main/java/com/example/connection/JdbcConnection.java
@@ -0,0 +1,49 @@
+package com.example.connection;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static com.example.connection.JdbcConstant.*;
+
+public class JdbcConnection {
+
+    private static HikariDataSource dataSource;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(URL);
+        config.setUsername(USERNAME);
+        config.setPassword(PASSWORD);
+        config.setAutoCommit(true);
+        config.setMaximumPoolSize(10);
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public static void closeConnection(Connection connection) throws SQLException {
+        if (connection != null)
+            connection.close();
+    }
+
+    public static void closePreparedStatement(PreparedStatement preparedStatement) throws SQLException {
+        if (preparedStatement != null)
+            preparedStatement.close();
+    }
+
+    public static void closeResultSet(ResultSet resultSet) throws SQLException {
+        if (resultSet != null)
+            resultSet.close();
+    }
+
+}
---
+++ src/main/java/com/example/connection/JdbcConstant.java
@@ -0,0 +1,11 @@
+package com.example.connection;
+
+public class JdbcConstant {
+
+    public static final String URL = "jdbc:postgresql://localhost:5432/user-account-db";
+
+    public static final String USERNAME = "erfan";
+
+    public static final String PASSWORD = "123456789Erfan";
+
+}
